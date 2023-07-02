dbe40ece9f54149660cc13078d4bdbe426d32793
lucifer1708 <20bce091@nith.ac.in>
2023-07-02 19:04:11 +0530

    added project name and desc

--- src/core/app.py
+++ src/core/app.py
@@ -1,6 +1,7 @@
-from src.api.router import api_router
 from fastapi import FastAPI
 from fastapi.responses import UJSONResponse
+
+from src.api.router import api_router
 
 
 def get_app() -> FastAPI:
@@ -12,8 +13,8 @@ def get_app() -> FastAPI:
     :return: application.
     """
     app = FastAPI(
-        title="Hackathon Project",
-        description="",
+        title="Resume Analyser",
+        description="The objective of our project is to extract relevant information from resume and present it in structured format. This project involves various stages of data processing, analysis and integration to provide an efficient and effective solution for automating the recruitment process.",
         version="1.0",
         docs_url="/docs/",
         redoc_url="/api/redoc/",
