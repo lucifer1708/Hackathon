3613fc978927cbc5207b81e16435a67159a778aa
lucifer1708 <20bce091@nith.ac.in>
2023-07-02 07:54:16 +0530

    added docker configurations

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.10.6
+WORKDIR /app
+ENV PYTHONUNBUFFERED=1
+COPY requirements.txt /tmp/requirements.txt
+RUN pip install --no-cache-dir -r /tmp/requirements.txt
+COPY . /app
+EXPOSE 8000
+ENV PYTHONPATH=/app
---
+++ src/deps/utils.py
@@ -0,0 +1,18 @@
+import PyPDF2
+
+
+def read_pdf(file_path):
+    # Usage example
+    # pdf_file = "download.pdf"
+    # extracted_text = read_pdf(pdf_file)
+    # print(extracted_text)
+    with open(file_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        num_pages = len(reader.pages)
+        text = ""
+
+        for page_number in range(num_pages):
+            page = reader.pages[page_number]
+            text += page.extract_text()
+
+        return text
